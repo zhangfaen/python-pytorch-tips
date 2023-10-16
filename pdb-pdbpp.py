bd5563ff24e0ed3abdd437b100f9c12c7d08a75c
zhangfaen <zhangfaen@ainnovation.com>
2023-10-16 15:14:19 +0800

    update

--- pdb-pdbpp.py
+++ pdb-pdbpp.py
@@ -4,5 +4,4 @@
 # import pdb; pdb.set_trace()
 
 # copy paste below line and run in pdb, then get a new pi function to dir(obj) attributes
-def pi(obj): import pprint as pp;  pp.pprint([(name,type(getattr(obj,name))) for name in dir(obj)])
-def pii(obj, pattern):  import pprint as pp; import re; pp.pprint([(name,type(getattr(obj,name))) for name in dir(obj) if len(re.findall(pattern, name)) > 0])
+def pi(obj, pattern=""):  import pprint as pp; import re; pp.pprint([(f"callable:{callable(getattr(obj,name))}", name, type(getattr(obj,name))) for name in dir(obj) if len(re.findall(pattern, name)) > 0]);
