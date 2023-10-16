f584d10ff3d3ddcc40596f2d84cf635d2b52b4fa
zhangfaen <zhangfaen@ainnovation.com>
2023-10-16 15:28:37 +0800

    update

--- pdb-pdbpp.py
+++ pdb-pdbpp.py
@@ -4,4 +4,14 @@
 # import pdb; pdb.set_trace()
 
 # copy paste below line and run in pdb, then get a new pi function to dir(obj) attributes
+# example usage:
+# import builtins
+# # find all attributes of builtins, whose name starts with 'm'
+# pi(builtins, "^m") # output
+    # >>> pi(builtins, "^m")
+    # [('callable:True', 'map', <class 'type'>),
+    #  ('callable:True', 'max', <class 'builtin_function_or_method'>),
+    #  ('callable:True', 'memoryview', <class 'type'>),
+    #  ('callable:True', 'min', <class 'builtin_function_or_method'>)]
 def pi(obj, pattern=""):  import pprint as pp; import re; pp.pprint([(f"callable:{callable(getattr(obj,name))}", name, type(getattr(obj,name))) for name in dir(obj) if len(re.findall(pattern, name)) > 0]);
+
