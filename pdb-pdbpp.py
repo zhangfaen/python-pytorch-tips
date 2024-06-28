01564851e459de7927093bdd0444d8c23f7ac5cd
zhangfaen <zhangfaen@ainnovation.com>
2024-06-28 23:56:54 +0800

    update

--- pdb-pdbpp.py
+++ pdb-pdbpp.py
@@ -15,3 +15,19 @@
     #  ('callable:True', 'min', <class 'builtin_function_or_method'>)]
 def pi(obj, pattern=""):  import pprint as pp; import re; pp.pprint([(f"callable:{callable(getattr(obj,name))}", name, type(getattr(obj,name))) for name in dir(obj) if not name.startswith("_") and len(re.findall(pattern, name)) > 0]);
 
+import re
+from pprint import pprint 
+from typing import List
+
+
+def grep(pattern: str, lst: List[str]) -> List[str]:
+    return [item for item in lst if re.search(pattern, item)]
+
+# 示例列表
+data = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
+
+# 使用自定义的 grep 函数
+result = grep(r'a', data)
+pprint(result)
+
+
